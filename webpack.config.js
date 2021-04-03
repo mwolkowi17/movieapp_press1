01db68755c81fc5d64d04dcf468a7105552932d7
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2021-04-03 10:42:45 +0200

    rename main failes

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
+    filename: 'bundlemov1.js',
     path: path.resolve(__dirname, 'dist'),
   },
   mode: 'development'
